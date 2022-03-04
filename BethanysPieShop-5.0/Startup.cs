b6dcdc5a2051581d1d062f9cf45052bf8c2715db
Lotte BEELEN <lotte.beelen@capgemini.com>
2022-03-04 13:30:12 +0100

    Setting up an MVC application

--- BethanysPieShop-5.0/Startup.cs
+++ BethanysPieShop-5.0/Startup.cs
@@ -16,6 +16,7 @@ namespace BethanysPieShop_5._0
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +27,16 @@ namespace BethanysPieShop_5._0
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseHttpsRedirection();
+            //for using statuc files like images. Defauls searches in wwwroot
+            app.UseStaticFiles();
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
